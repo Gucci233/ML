96f7252a62a08171b1968f6195f9b39f207b850b
prinsyin <1914001753@qq.com>
2023-03-12 15:30:30 +0800

    poly

--- MultivariateLinearRegression.py
+++ MultivariateLinearRegression.py
@@ -5,9 +5,68 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.neural_network import MLPRegressor
 from sklearn.linear_model import LinearRegression,LogisticRegression,LassoCV
 import lightgbm as lgb
-
+from itertools import combinations_with_replacement
 def sigmoid(x):
     return 1.0/(1+np.exp(-x))
+
+
+def polynomial_features(X, degree=2):
+    n_samples, n_features = X.shape
+    X=np.array(X)
+    combinations=[]
+    for d in range(1,degree+1):
+        combinations+=[comb for comb in combinations_with_replacement(range(n_features), d)]
+    
+    n_output_features = len(combinations)+1
+    print(combinations,n_output_features)
+    PF_array = np.empty((n_samples, n_output_features))
+    PF_array[:, 0] = 1
+    for i, comb in enumerate(combinations):
+        PF_array[:, i+1] = X[:, comb].prod(1)
+
+    return PF_array
+
+class PolynomialRegression:
+    def __init__(self, learning_rate=0.01, n_iterations=2000, n_folds=5):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.n_folds = n_folds
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        X_poly = polynomial_features(X, degree=2)
+        print(type(X_poly))
+        n_samples, n_features = X_poly.shape
+        print(X.shape,X_poly.shape)
+        self.weights =np.ones(n_features).reshape(-1,1)
+        self.bias = 0
+        
+        kf = KFold(n_splits=self.n_folds, shuffle=True)
+        #交叉验证
+        for i in range(self.n_iterations):
+            for train_idx, val_idx in kf.split(X_poly):
+                X_train, y_train = X_poly[train_idx], y[train_idx]
+                X_val, y_val = X_poly[val_idx], y[val_idx]
+                X_train=np.array(X_train)
+                y_train=np.array(y_train).reshape(-1,1)
+                X_val=np.array(X_val)
+                y_val=np.array(y_val).reshape(-1,1)
+                y_pred_train = np.dot(X_train, self.weights) + self.bias
+                y_pred_val = np.dot(X_val, self.weights) + self.bias
+                train_loss = np.mean(np.square(y_pred_train- y_train))
+                val_loss = np.mean(np.square(y_pred_val - y_val))
+                dw = (1 / n_samples) * np.dot(X_train.T, (y_pred_train - y_train))#线性回归weight梯度
+                db = (2 / n_samples) * np.sum(y_pred_train - y_train)#bias梯度
+                self.weights -= self.learning_rate * dw
+                self.bias -= self.learning_rate * db
+            if i % 100 == 0:
+                print(f"Iteration {i}, train_loss={train_loss:.4f}, val_loss={val_loss:.4f}")
+
+    def predict(self, X):
+        X_poly = polynomial_features(X, degree=2)
+        y_pred = np.dot(X_poly, self.weights) + self.bias
+        return y_pred
 
 class MultivariateLinearRegression:
     def __init__(self, learning_rate=0.01, n_iterations=2000, n_folds=5):
@@ -167,11 +226,13 @@ for f in x_train:
         x_train[f]=(x_train[f]-np.min(x_train[f]))/(np.max(x_train[f])-np.min(x_train[f]))
         x_test[f]=(x_test[f]-np.min(x_test[f]))/(np.max(x_test[f])-np.min(x_test[f]))
         
-        
-model= MultivariateLinearRegression()
-model1=LassoRegression(Lambda=36,iteration_method='CDT')
-model1.fit(x_train,y_train)
-y_test=model1.predict(x_test)
+model= PolynomialRegression()
+model.fit(x_train,y_train)
+# model= MultivariateLinearRegression()
+# model.fit(x_train,y_train)
+# model1=LassoRegression(Lambda=36,iteration_method='CDT')
+# model1.fit(x_train,y_train)
+y_test=model.predict(x_test)
 print('手写模型预测结果：')
 print(y_test)
 
